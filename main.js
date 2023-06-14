5a5fbcb7978dd8f23eb7261f84103a65aa477382
Zarubin Alexander <zarubin_14@list.ru>
2023-06-14 17:50:27 +0300

    Add scrollReaveal

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x')
     navlist.classList.toggle('open')
 }
+
+const scrollReveal = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+scrollReveal.reveal('.heroes-text', {delay: 400, origin: 'top'});
+scrollReveal.reveal('.hero-one', {delay: 200, origin: 'left'});
+scrollReveal.reveal('.hero-two', {delay: 200, origin: 'right'});
